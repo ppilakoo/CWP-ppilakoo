22c823dca4cab81e1d5e08d987d920f8e3844dc2
ppilakoo <68010795@kmitl.ac.th>
2026-09-22 16:58:41 +0700

    Add files via upload

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+stnum = int(input("Give me the first number: ").strip())
+ndnum = int(input("Give me the second number: ").strip())
+print("Thank you!")
+print(f"{stnum} + {ndnum} = {stnum+ndnum}")
+print(f"{stnum} - {ndnum} = {stnum-ndnum}")
+print(f"{stnum} / {ndnum} = {stnum//ndnum}")
+print(f"{stnum} * {ndnum} = {stnum*ndnum}")
