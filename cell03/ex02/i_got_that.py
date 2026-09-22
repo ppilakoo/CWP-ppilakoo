7f2a6940eb9612f752c352d811a3ec273a08ddf3
ppilakoo <68010795@kmitl.ac.th>
2026-09-22 16:13:31 +0700

    Add files via upload

---
+++ cell03/ex02/i_got_that.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+say = input("What you gotta say? : ").strip()
+while True:
+        if say == "STOP":
+            break
+        say = input("I got that! Anything else? : ").strip()
